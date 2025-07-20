b09690bbd8673b80010a2cab526ecb960a487d3c
Pranit Panda <pandapranit566@gmail.com>
2025-07-20 21:55:47 +0530

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:truxoo_partners/splash_screen.dart';
-import 'driver_sign_up_page.dart';
-import 'login_page.dart';
+
 void main() {
   runApp(
     MaterialApp(
-      home: Login(),
+      home: TypingSplashScreen(),
   ));
 }
 //ready
